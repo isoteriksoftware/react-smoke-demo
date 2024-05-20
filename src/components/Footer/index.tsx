848a47352ea503e17aa2b2385a655f0c950ebfd7
imran <isoteriksoftware@gmail.com>
2024-05-20 13:21:17 +0100

    added repo link and readme updates

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import githubIcon from "@/assets/github.svg";
+
 const GITHUB_USERNAME = "isoteriksoftware";
 const LIBRARY_NAME = "react-smoke";
+const DEMO_REPO = "react-smoke-demo";
 
 export const Footer = () => {
   return (
@@ -9,34 +12,52 @@ export const Footer = () => {
         background: "#292828",
         width: "100%",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        columnGap: "10px",
+        alignItems: "center",
+        padding: "10px",
         paddingTop: "16px",
-        paddingBottom: "10px",
       }}
     >
-      <a href={`https://www.npmjs.com/package/${LIBRARY_NAME}`} target="_blank">
-        <img
-          src={`https://img.shields.io/npm/v/${LIBRARY_NAME}.svg`}
-          alt="NPM"
-        />
-      </a>
-
-      <img
-        src={`https://img.shields.io/npm/dt/${LIBRARY_NAME}.svg`}
-        alt="NPM"
-      />
-
       <a
-        href={`https://github.com/${GITHUB_USERNAME}/${LIBRARY_NAME}`}
+        href={`https://github.com/${GITHUB_USERNAME}/${DEMO_REPO}`}
         target="_blank"
       >
-        <img
-          src={`https://img.shields.io/github/stars/${GITHUB_USERNAME}/${LIBRARY_NAME}?style=social`}
-          alt="GitHub stars"
-        />
+        <img src={githubIcon.src} alt="Demo repo" width="40" height="40" />
       </a>
+
+      <div
+        style={{
+          display: "flex",
+          columnGap: "10px",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          flexGrow: 1,
+        }}
+      >
+        <a
+          href={`https://www.npmjs.com/package/${LIBRARY_NAME}`}
+          target="_blank"
+        >
+          <img
+            src={`https://img.shields.io/npm/v/${LIBRARY_NAME}.svg`}
+            alt="NPM"
+          />
+        </a>
+
+        <img
+          src={`https://img.shields.io/npm/dt/${LIBRARY_NAME}.svg`}
+          alt="NPM"
+        />
+
+        <a
+          href={`https://github.com/${GITHUB_USERNAME}/${LIBRARY_NAME}`}
+          target="_blank"
+        >
+          <img
+            src={`https://img.shields.io/github/stars/${GITHUB_USERNAME}/${LIBRARY_NAME}?style=social`}
+            alt="GitHub stars"
+          />
+        </a>
+      </div>
     </footer>
   );
 };
